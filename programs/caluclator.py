56db30ed66d6f2031141c8840f42a38576e89271
vishnu123-ops <koppoluvishuvardhan@gmail.com>
2025-01-29 21:38:12 +0530

    sixth commit

---
+++ programs/caluclator.py
@@ -0,0 +1,42 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error! Division by zero."
+    return a / b
+
+
+while True:
+    print("\nSimple Calculator")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter choice (1-5): ")
+
+    if choice == '5':
+        print("Exiting Calculator. Goodbye!")
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    elif choice == '4':
+        print("Result:", divide(num1, num2))
+    else:
+        print("Invalid choice! Please enter a valid option.")

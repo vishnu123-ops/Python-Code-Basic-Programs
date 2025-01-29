dad42a7ea69451c970cde90820da65937e311a12
vishnu123-ops <koppoluvishuvardhan@gmail.com>
2025-01-29 21:29:51 +0530

    fourth commit

---
+++ programs/datatypes.py
@@ -0,0 +1,34 @@
+
+int_var = 10
+print("Integer:", int_var, "| Type:", type(int_var))
+
+
+float_var = 10.5
+print("Float:", float_var, "| Type:", type(float_var))
+
+
+str_var = "Hello, Python!"
+print("String:", str_var, "| Type:", type(str_var))
+
+
+bool_var = True
+print("Boolean:", bool_var, "| Type:", type(bool_var))
+
+list_var = [1, 2, 3, "apple", "banana"]
+print("List:", list_var, "| Type:", type(list_var))
+
+
+tuple_var = (10, 20, 30, "cat", "dog")
+print("Tuple:", tuple_var, "| Type:", type(tuple_var))
+
+
+set_var = {1, 2, 3, 3, 4, 5}
+print("Set:", set_var, "| Type:", type(set_var))
+
+
+dict_var = {"name": "Alice", "age": 25, "city": "New York"}
+print("Dictionary:", dict_var, "| Type:", type(dict_var))
+
+
+none_var = None
+print("NoneType:", none_var, "| Type:", type(none_var))
